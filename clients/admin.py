7b92d3bd0f9226f6f088520c389e4974c16c293a
Adson Rodrigues <adson.develop@gmail.com>
2020-08-30 11:02:21 -0300

    feat: Profile customization in django admin

--- clients/admin.py
+++ clients/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class CompanyAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'first_name', 'email', 'company', 'title')
+    list_display = ('id', 'first_name', 'email', 'company', 'title')
+    list_filter = ('company',)
+    ordering = ('first_name',)
