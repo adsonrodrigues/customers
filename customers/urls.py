93b1473b4e52c4a60bca6638ab595bb41c388410
Adson Rodrigues <adson.develop@gmail.com>
2020-08-31 19:45:21 -0300

    feat: add app api in router

--- customers/urls.py
+++ customers/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+site_title = 'Customers'
+admin.site.site_header = site_title
+admin.site.site_title = site_title
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(('api.urls', 'api'), namespace='api')),
 ]
